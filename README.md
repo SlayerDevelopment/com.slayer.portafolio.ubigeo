57270c6cb186beda49bdca3ffbf686482f37e1db
Bryan Antonio Alvarado Villalobos <slayer-dev@pop-os.localdomain>
2019-12-30 18:46:38 -0500

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# com.slayer.portafolio.ubigeo
+## Entorno
+### Java 8 Oracle
+### SpringBoot 2.2.2
+### H2 database
+
+## Primera version V1
+### Se mantiene lo del entorno
---
+++ src/test/java/com/slayer/portafolio/ubigeo/main/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slayer.portafolio.ubigeo.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
